a290669a50dee2971d22b268ecd3a6856bc01d3a
Lee0568 <shengshengdasha@foxmail.com>
2025-08-01 07:16:47 +0800

    文件

---
+++ src/main/java/net/thekingofduck/loki/entity/SshServerEntity.java
@@ -0,0 +1,20 @@
+package net.thekingofduck.loki.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@ToString
+@Component
+public class SshServerEntity {
+    private int id;
+    private String host;
+    private int port;
+    private String username;
+    private String password; // 加密存储
+    private String remark;
+    private String createTime;
+}
